31de567c99b46ac7cef926001c92fb1ee3e00d71
DeepeshKalura <deepeshkalurs@gmail.com>
2025-04-09 15:15:13 +0530

    Update app.listen to bind to '0.0.0.0' for external access

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`App listening at http://localhost:${port}`);
 });
